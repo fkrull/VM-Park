cece3d17d9d53c86a870d9d567a6255f935ee58b
Felix Krull <f_krull@gmx.de>
2020-08-16 14:09:07 +0200

    Fix kernel upgrades on newer Fedoras

--- provision/install-desktop-fedora.sh
+++ provision/install-desktop-fedora.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dnf --exclude kernel,kernel-core,kernel-modules,kernel-modules-extra -y \
+dnf --exclude kernel,kernel-core,kernel-debug,kernel-debug-core,kernel-modules,kernel-modules-extra -y \
     --allowerasing \
     group install "Fedora Workstation"
 systemctl enable gdm
