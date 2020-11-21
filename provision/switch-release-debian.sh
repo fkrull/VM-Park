d6efd7a6351a5f76d006c1ee27a83024efad5da3
Felix Krull <f_krull@gmx.de>
2020-11-21 22:01:05 +0100

    Rebuild guest additions maybe better

--- provision/switch-release-debian.sh
+++ provision/switch-release-debian.sh
@@ -11,19 +11,16 @@ deb https://deb.debian.org/debian/ ${RELEASE} main contrib
 deb-src https://deb.debian.org/debian/ ${RELEASE} main contrib
 EOF
 
+cat | debconf-set-selections <<EOF
+grub-pc grub-pc/install_devices multiselect /dev/sda
+EOF
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -y dist-upgrade
+apt-get -y dist-upgrade || apt-get -y dist-upgrade
 apt-get -y --purge autoremove
 
-# fix bootloader
-grub-install /dev/sda
-
-# determine newest kernel
-kernel=$(ls -1 /lib/modules | sort --version-sort | tail -1)
-apt-get -y install linux-headers-${kernel}
 
 # rebuild VBox guest additions modules
-vbox_dir=$(ls /opt | grep VBoxGuestAdditions)
-vbox_src_dir=$(ls /opt/${vbox_dir}/src | grep vboxguest)
-make -C /opt/${vbox_dir}/src/${vbox_src_dir} KERN_VER=${kernel} vboxsf
+apt-get -y install linux-headers-amd64 build-essential
+rcvboxadd quicksetup all
